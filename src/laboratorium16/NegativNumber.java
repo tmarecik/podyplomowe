6a5018caf67d14c53ea36d44c61a11c9053592f2
tmarecik <tmarecik@wszib.edu.pl>
2020-01-12 08:31:56 +0100

    zajecia 15.12

---
+++ src/laboratorium16/NegativNumber.java
@@ -0,0 +1,29 @@
+package laboratorium16;
+
+import java.awt.*;
+
+class NegativNumberException extends Exception {
+    public NegativNumberException() {
+    }
+
+    public NegativNumberException(String message) {
+        super(message);
+    }
+
+    //    long number;
+//
+//    public NegativNumber(long number) {
+//        this.number = number;
+//        this.checkNegativNumber(this.number);
+//    }
+//
+//    public boolean checkNegativNumber(long number) {
+//        if (number < 0) {
+//            throw new NegativNumber("Akcepted only positiv numbers");
+//        } else {
+//            return true;
+//        }
+//    }
+//}
+//
+}
---
+++ src/laboratorium16/PrimeFinder.java
@@ -0,0 +1,43 @@
+package laboratorium16;
+
+public class PrimeFinder implements Runnable {
+    public long target;
+    public long prime;
+    public boolean finished = false;
+    private Thread runner;
+
+    PrimeFinder(long inTarget) throws NegativNumberException {
+        if(inTarget < 0){
+            throw new NegativNumberException("Podano liczbe ujemna!");
+        }
+
+        target = inTarget;
+
+        if (runner == null) {
+            runner = new Thread(this);
+            runner.start();
+        }
+    }
+
+    public void run() {
+        long numPrimes = 0;
+        long candidate = 2;
+        while (numPrimes < target) {
+            if (isPrime(candidate)) {
+                numPrimes++;
+                prime = candidate;
+            }
+            candidate++;
+        }
+        finished = true;
+    }
+
+    boolean isPrime(long checkNumber) {
+        double root = Math.sqrt(checkNumber);
+        for (int i = 2; i <= root; i++) {
+            if (checkNumber % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
